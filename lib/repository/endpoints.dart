89ae58e549bdeaba67cdca55209301cec555ac29
DiogoSequeira <diogosequeira@hotmail.fr>
2022-10-01 00:21:40 +0100

    add devPrivateKeyValue

--- lib/repository/endpoints.dart
+++ lib/repository/endpoints.dart
@@ -9,5 +9,5 @@ class Endpoints {
   static String wsUrl() => 'ws://127.0.0.1:7545';
 
   /// Never use private keys in production, use walletconnect + metamask instead
-  static String mockPrivateGanashKey() => 'loremipsum';
+  static String mockPrivateGanashKey() => 'b8cf2cb2a5f2c366f7a41222dd3d77e3e3e80ad86225ee98db3c59a584934564';
 }
