0a8d1dac1a77d6905c2518a1e41491685ae2a4bc
DiogoSequeira <diogosequeira@hotmail.fr>
2022-09-30 17:59:37 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: [],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
