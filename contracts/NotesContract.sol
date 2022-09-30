ab2ebd4dfae85303b9277fbd942885c80c378006
DiogoSequeira <diogosequeira@hotmail.fr>
2022-09-30 23:55:40 +0100

    refactor faulty NotesContract.sol

--- contracts/NotesContract.sol
+++ contracts/NotesContract.sol
@@ -12,15 +12,15 @@ contract NotesContract {
     mapping(uint256 => Note) public notes;
 
     event NoteCreated(uint256 id, string title, string description);
-    event NoteCreated(uint256 id);
+    event NoteDeleted(uint256 id);
 
-    function createNote(string memory _title, string memory _description){
+    function createNote(string memory _title, string memory _description) public {
         notes[noteCount] = Note(noteCount, _title, _description);
         emit NoteCreated(noteCount, _title, _description);
         noteCount++;
     }
 
-    function deleteNote(uint256 _id) public{
+    function deleteNote(uint256 _id) public {
         delete notes[_id];
         emit NoteDeleted(_id);
         noteCount--;
