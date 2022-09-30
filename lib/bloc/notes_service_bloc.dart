04e3b26e400577cc685bfd3df9d3dfac26cbf4ca
DiogoSequeira <diogosequeira@hotmail.fr>
2022-09-30 20:25:52 +0100

    added Address, web3client, contractAbiCode and credentials

--- lib/bloc/notes_service_bloc.dart
+++ lib/bloc/notes_service_bloc.dart
@@ -11,14 +11,6 @@ part 'notes_service_state.dart';
 class NotesServiceBloc extends Bloc<NotesServiceEvent, NotesServiceState> {
   final NotesRepository notesRepository;
   NotesServiceBloc({required this.notesRepository}) : super(NotesServiceInitial()) {
-    /// Remote Procedure Call [RPC Server] Ganash
-    final _rpcUrl = 'http://127.0.0.1:7545';
-
-    /// WebSocket Url
-    final _wsUrl = 'ws://127.0.0.1:7545';
-
-    /// Never use private keys in production, use walletconnect + metamask instead
-    final _mockPrivateGanashKey = 'loremipsum';
     List<Note> notes = [];
 
     on<NotesServiceEvent>((event, emit) {});
--- lib/repository/notes_repository.dart
+++ lib/repository/notes_repository.dart
@@ -1,11 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/io.dart';
 
 class NotesRepository {
-  final Web3Client web3client;
-  final httpClient = http.Client();
+  late Web3Client web3client;
+  late ContractAbi contractAbiCode;
+  late EthereumAddress contractAddress;
+  late EthPrivateKey credentials;
 
-  NotesRepository({
-    required this.web3client,
-  });
+  /// Remote Procedure Call [RPC Server] Ganash
+  final _rpcUrl = 'http://127.0.0.1:7545';
+
+  /// WebSocket Url
+  final _wsUrl = 'ws://127.0.0.1:7545';
+
+  /// Never use private keys in production, use walletconnect + metamask instead
+  final _mockPrivateGanashKey = 'loremipsum';
+  static const notesContractPath = 'build/contracts/NotesContract.json';
+
+  NotesRepository() {
+    web3client = Web3Client(
+      _rpcUrl,
+      http.Client(),
+      socketConnector: () {
+        return IOWebSocketChannel.connect(_wsUrl).cast<String>();
+      },
+    );
+  }
+
+  Future<void> getABI() async {
+    final abiFile = await rootBundle.loadString(notesContractPath);
+    final jsonABI = jsonDecode(abiFile);
+    contractAbiCode =
+        ContractAbi.fromJson(jsonEncode(jsonABI['abi']), 'NotesContract');
+  }
+
+  Future<void> getAddress(ContractAbi contractAbi) async {
+    final abiFile = await rootBundle.loadString(notesContractPath);
+    final jsonABI = jsonDecode(abiFile);
+    contractAddress =
+        EthereumAddress.fromHex(jsonABI["networks"]["5777"]["address"]);
+  }
+
+  Future<void> getCredentials() async {
+    credentials = EthPrivateKey.fromHex(_mockPrivateGanashKey);
+  }
 }
