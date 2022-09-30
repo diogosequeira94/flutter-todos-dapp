528ff5aea9b1f2a120cb8eea1914b41072080691
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-09-30 17:56:52 +0100

    removed unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Todos Dapp'),
       ),
       body: Center(
         child: Column(
