164f0003da5cc1d0e1115b03c8cc6b03e1ed8faa
DiogoSequeira <diogosequeira@hotmail.fr>
2022-10-02 00:03:27 +0100

    notes deployed contract

--- lib/repository/notes_deployed_contract.dart
+++ lib/repository/notes_deployed_contract.dart
@@ -1,16 +1,14 @@
 import 'package:web3dart/web3dart.dart';
 
 class NotesDeployedContract {
-  final DeployedContract deployedContract;
+  late DeployedContract deployedContract;
   late ContractFunction createNote;
   late ContractFunction deleteNote;
   late ContractFunction notes;
   late ContractFunction noteCount;
 
-  NotesDeployedContract(this.deployedContract){
-    initFunctions();
-  }
-  Future<void> initFunctions() async {
+  Future<void> initContract(ContractAbi contractAbiCode, EthereumAddress contractAddress) async {
+    deployedContract = DeployedContract(contractAbiCode, contractAddress);
     createNote = deployedContract.function('createNote');
     deleteNote = deployedContract.function('deleteNote');
     notes = deployedContract.function('notes');
